ef35ed6300770f5087cd05e3b5473435f31c0cff
davidlgabriel <david.g.leal00@gmail.com>
2023-07-14 09:15:34 +0100

    image

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    O seu Vizinho
+                    <img src="oseuvizinho.jpeg" style="height:3.5rem;" alt="">
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
